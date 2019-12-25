355971c95c5ca50c925d386899a0f9dc25ad0b29
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-25 23:07:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  pp director_data
+  #pp director_data
   total_of_worldwide_grosses = 0
 
   for i in 0...director_data[:movies].length
