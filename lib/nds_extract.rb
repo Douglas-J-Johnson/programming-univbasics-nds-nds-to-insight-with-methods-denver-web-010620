eaba28646f3a41c6342590b715a98b56d4610661
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-25 22:39:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,9 +5,9 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+
   pp director_data
-  
+
 end
 
 # Write a method that, given an NDS creates a new Hash
